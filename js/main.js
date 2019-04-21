fdebe98c59894a52e7956ccb25e883f0fb90ace8
watepu <40508321+watepu@users.noreply.github.com>
2019-04-21 18:26:33 +0900

    オブジェクトの要素の取り出し、追加、変更、削除

--- js/main.js
+++ js/main.js
@@ -8,4 +8,15 @@
   };
 
   console.log(player);
+  console.log(player.name);
+  console.log(player['name']);
+
+  player.score = 100;
+  console.log(player);
+
+  player.age = 30;
+  console.log(player);
+
+  delete player.age;
+  console.log(player);
 }
