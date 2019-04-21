d8d7dc646fa9c28e818aa21d916f711a3f97d753
watepu <40508321+watepu@users.noreply.github.com>
2019-04-21 19:20:52 +0900

    クラスの継承

--- js/main.js
+++ js/main.js
@@ -7,10 +7,24 @@
       this.name = name;
       this.score = score;
     }
+    showInfo(){
+      console.log(`name:${this.name} score:${this.score}`)
+    }
   }
 
-  const watepu = new Player('watepu', 80);
-  const neko = new Player('neko', 60);
+  class SoccerPlayer extends Player{
+    constructor(name, score, number){
+      super(name, score);
+      this.number = number;
+    }
+    kick(){
+      console.log('Goal!');
+    }
+  }
 
-  console.log(watepu.score); //80
+  const watepu = new SoccerPlayer('watepu', 80, 5);
+
+  watepu.kick(); //Goal!
+  console.log(watepu.number); //5
+  watepu.showInfo(); //name:watepu score:80
 }
