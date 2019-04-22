b74bdabbc5fb28cae9626031a361a2a211f53181
watepu <40508321+watepu@users.noreply.github.com>
2019-04-22 21:06:34 +0900

    map,filterの使い方

--- js/main.js
+++ js/main.js
@@ -2,15 +2,20 @@
 // 厳密なエラーチェックをしてくれる
 
 {
-  const a = [1, 5, 10];
-// アロー関数を使用
-  a.forEach(item => {
-    console.log(item);
-  });
-  //1 5 10が続けて出力
+  const a = [1, 2, 5, 10];
 
-  a.forEach((item, index) => {
-    console.log(`${index}:${item}`);
-  });
-  //0:1 1:5 2:10が続けて出力
+  // それぞれの数を２倍にしたい
+
+  // const b = a.map(item => {
+  //   return item * 2;
+  // });
+  // return１行なので下のように書き換えられる
+  const b = a.map(item => item * 2);
+  console.log(b);
+
+  // 偶数の要素だけを抜き出したい
+  // 偶数の時だけtrueを返す
+  const c = a.filter(item => item % 2 === 0);
+  console.log(c);
+
 }
