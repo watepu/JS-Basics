e4532f565b3845e7645d65cb887b5b1035b0c449
watepu <40508321+watepu@users.noreply.github.com>
2019-04-22 21:17:24 +0900

    オブジェクトの操作

--- js/main.js
+++ js/main.js
@@ -2,20 +2,14 @@
 // 厳密なエラーチェックをしてくれる
 
 {
-  const a = [1, 2, 5, 10];
+  const o = {
+    a: 1,
+    b: 2,
+  };
 
-  // それぞれの数を２倍にしたい
+  console.log(Object.keys(o)); //キーの一覧を配列としてとってくる ['a', 'b']
+  console.log(Object.values(o)); //値の一覧を配列としてとってくる [1, 2]
+  console.log(Object.entries(o)); //全体を配列としてとってくる [['a', 1], ['b', 2]]
 
-  // const b = a.map(item => {
-  //   return item * 2;
-  // });
-  // return１行なので下のように書き換えられる
-  const b = a.map(item => item * 2);
-  console.log(b);
-
-  // 偶数の要素だけを抜き出したい
-  // 偶数の時だけtrueを返す
-  const c = a.filter(item => item % 2 === 0);
-  console.log(c);
 
 }
