d24fa663db471e10a996a85b01b34fbc7165f5d9
watepu <40508321+watepu@users.noreply.github.com>
2019-04-22 20:58:33 +0900

    forEachの使い方

--- js/main.js
+++ js/main.js
@@ -3,10 +3,14 @@
 
 {
   const a = [1, 5, 10];
+// アロー関数を使用
+  a.forEach(item => {
+    console.log(item);
+  });
+  //1 5 10が続けて出力
 
-  a.splice(2, 0, 6, 7);  //indexが2の前、削除するのは0、追加するのは6、7
-  console.log(a);  //[1, 5, 6, 7, 10]
-
-  a.splice(1, 2);  //indexが1のところから2つ削除
-  console.log(a);  //[1, 7, 10]
+  a.forEach((item, index) => {
+    console.log(`${index}:${item}`);
+  });
+  //0:1 1:5 2:10が続けて出力
 }
