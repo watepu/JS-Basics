3ad6aea349aab28a83b23f5290d69916b7fcbf62
watepu <40508321+watepu@users.noreply.github.com>
2019-04-23 20:30:03 +0900

    スプレッド演算子の使い方

--- js/main.js
+++ js/main.js
@@ -2,14 +2,8 @@
 // 厳密なエラーチェックをしてくれる
 
 {
-  const o = {
-    a: 1,
-    b: 2,
-  };
-
-  console.log(Object.keys(o)); //キーの一覧を配列としてとってくる ['a', 'b']
-  console.log(Object.values(o)); //値の一覧を配列としてとってくる [1, 2]
-  console.log(Object.entries(o)); //全体を配列としてとってくる [['a', 1], ['b', 2]]
-
+  const a = [10, 20];
+  const b = [1, 2, ...a];
+  console.log(b); //[1, 2, 10, 20]
 
 }
